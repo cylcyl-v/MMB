b4280d5ebce33a93cb24c8994d18b53b2ba4007c
chenyili0818 <lichenyi@stu.pku.edu.cn>
2025-10-25 10:01:13 +0800

    update

--- html_gen.sh
+++ html_gen.sh
@@ -1,5 +1,5 @@
 python html_gen.py \
-  --xlsx /pfs/lichenyi/station/VLMEval/outputs/mmbench_dev_en/UnifyModelEval/train_qwendit_unify_interleave_stage1p5/0000050000/UnifyModelEval_MMBench_DEV_EN.xlsx \
+  --xlsx /pfs/lichenyi/station/VLMEval/outputs/mmbench_dev_en/UnifyModelEval/train_qwendit_unify_interleave_debug_vqa/0000020000/UnifyModelEval_MMBench_DEV_EN.xlsx\
   --image-root /pfs/shared_eval/datasets/images/MMBench \
   --out-dir ./ \
   --image-prefix assets/mmbench
